3d9bbec31a1d9887f26f285a9c24515e413822d4
BrianWu <brianwu291@gmail.com>
2024-12-20 23:23:21 +0800

    feat: add model types for fakeStore response

--- types/types.go
+++ types/types.go
@@ -19,4 +19,20 @@ type (
 		Profit float64 `json:"profit" binding:"required"`
 		Ratio  float64 `json:"ratio" binding:"required"`
 	}
+
+	Category string
+	Product  struct {
+		ID          int64    `json:"id"`
+		Title       string   `json:"title"`
+		Description string   `json:"description"`
+		Price       float64  `json:"price"`
+		Category    Category `json:"category"`
+		Image       string   `json:"image"`
+		Rating      ProductRating
+	}
+
+	ProductRating struct {
+		Rate  float64 `json:"rate"`
+		Count int64   `json:"count"`
+	}
 )

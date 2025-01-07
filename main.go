0db12c6e992103099c58a3515f3eee67be3c55a7
BrianWu <brianwu291@gmail.com>
2025-01-08 01:02:19 +0800

    feat: adapt psql client in main.go

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	dotEnv "github.com/joho/godotenv"
 
 	cache "github.com/brianwu291/go-learn/cache"
+	postgres "github.com/brianwu291/go-learn/db/postgres"
 	redis "github.com/brianwu291/go-learn/db/redis"
 
 	ratelimiter "github.com/brianwu291/go-learn/middlewares/ratelimiter"
@@ -21,6 +22,9 @@ import (
 	fakestorehandler "github.com/brianwu291/go-learn/handlers/fakestore"
 	fakestorerepo "github.com/brianwu291/go-learn/repos/fakestore"
 	fakestoreservice "github.com/brianwu291/go-learn/services/fakestore"
+
+	constants "github.com/brianwu291/go-learn/constants"
+	types "github.com/brianwu291/go-learn/types"
 )
 
 var (
@@ -53,10 +57,18 @@ func main() {
 	// Using %+v - shows field names
 	// Using %#v - shows type information and field names
 
+	postgresConfig := postgres.LoadConfigFromEnv()
+	postgresDB, dbInitErr := postgres.New(postgresConfig)
+	if dbInitErr != nil {
+		fmt.Printf("failed to initialize postgres database: %+v\n", dbInitErr)
+		return
+	}
+	defer postgresDB.Close()
+
 	redisDBStr := os.Getenv("REDIS_DB")
 	redisDB, err := strconv.Atoi(redisDBStr)
 	if err != nil {
-		fmt.Printf("error on parsing REDIS_DB env value: %+v. REDIS_DB: %+v", err.Error(), redisDBStr)
+		fmt.Printf("error on parsing REDIS_DB env value: %+v. REDIS_DB: %+v\n", err.Error(), redisDBStr)
 		return
 	}
 
@@ -68,7 +80,7 @@ func main() {
 	}
 	cacheClient, err := redis.NewClient(cacheConfig)
 	if err != nil {
-		fmt.Printf("Failed to initialize Redis cache: %v", err)
+		fmt.Printf("failed to initialize Redis cache: %v\n", err)
 		return
 	}
 
@@ -76,6 +88,18 @@ func main() {
 	rateLimiter := ratelimiter.NewRateLimiter(cacheClient)
 
 	r := gin.Default()
+
+	r.Use(postgresDB.Middleware())
+	r.GET("/health", func(c *gin.Context) {
+		if healthyErr := postgresDB.Health(); healthyErr != nil {
+			fmt.Printf("error on postgres healthy check: %+v. error: %+v\n", healthyErr.Error(), healthyErr)
+			internalServerErr := fmt.Errorf(constants.InternalServerErrorMessage)
+			c.JSON(http.StatusInternalServerError, types.InternalServerErrorResponse{Message: internalServerErr.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
 	financialService := financialservice.NewFinancialService()
 	financialHandler := financialhandler.NewFinancialHandler(financialService)
 
